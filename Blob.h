28f580d3e1a6eb05f1839ac32c5a55cd51eb5dbe
LLC <2952249022@qq.com>
2023-04-16 19:32:56 +0800

    Update Blob.h
    blob.h添加中文注释

--- Blob.h
+++ Blob.h
@@ -1,6 +1,6 @@
 // Blob.h
 
-#ifndef MY_BLOB
+#ifndef MY_BLOB // 如果 MY_BLOB 没有被定义过，则继续编译
 #define MY_BLOB
 
 #include<opencv2/core/core.hpp>
@@ -13,26 +13,28 @@ public:
     // member variables ///////////////////////////////////////////////////////////////////////////
     std::vector<cv::Point> currentContour;
 
-    cv::Rect currentBoundingRect;
+    cv::Rect currentBoundingRect;   // 一个 Blob 的外接矩形
 
-    std::vector<cv::Point> centerPositions;
+    std::vector<cv::Point> centerPositions;   // 一个 Blob 中心点的位置向量
 
-    double dblCurrentDiagonalSize;
-    double dblCurrentAspectRatio;
+    double dblCurrentDiagonalSize;   // 当前 Blob 的对角线长度
 
-    bool blnCurrentMatchFoundOrNewBlob;
+    double dblCurrentAspectRatio;   // 当前 Blob 的长宽比
 
-    bool blnStillBeingTracked;
+    bool blnCurrentMatchFoundOrNewBlob;   // 表示当前 Blob 是否与之前的 Blob 匹配
 
-    int intNumOfConsecutiveFramesWithoutAMatch;
+    bool blnStillBeingTracked;   // 表示当前 Blob 是否仍然被跟踪
 
-    cv::Point predictedNextPosition;
+    int intNumOfConsecutiveFramesWithoutAMatch;   // 一系列连续帧中，未匹配的 Blob 的数量
+
+    cv::Point predictedNextPosition;   // 预测当前 Blob 的下一个位置
 
     // function prototypes ////////////////////////////////////////////////////////////////////////
-    Blob(std::vector<cv::Point> _contour);
-    void predictNextPosition(void);
+    Blob(std::vector<cv::Point> _contour);   // Blob 类的构造函数
 
+    void predictNextPosition(void);   // 预测当前 Blob 的下一个位置，并将其存储在 predictedNextPosition 中
 };
 
-#endif    // MY_BLOB
+#endif // MY_BLOB
 
+#pragma once // 指示编译器只需要在一个文件中包含这个头文件
